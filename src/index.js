165a47109736fc7c937782f5946be9e8a5244b03
Andrew <andrewstaveley14@gmail.com>
2023-10-03 20:32:30 -0400

    Completed deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", (e) => {
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDeleteToDo)
+  p.textContent = `${todo} `
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDeleteToDo (e) {
+  e.target.parentNode.remove()
+}
